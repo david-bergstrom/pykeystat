2b5c0a3292896375f70184e15e2beb34955b3586
David Bergström <david-bergstrom@outlook.com>
2016-08-29 19:02:26 +0200

    Remove prints

--- sqliterecorder.py
+++ sqliterecorder.py
@@ -1,5 +1,6 @@
 # A simple recorder which saves the records into a SQLite3 database with
 # a matching timestamp of when the keystroke was recorded".
+
 import sqlite3
 
 class SQLiteRecorder:
@@ -9,11 +10,8 @@ class SQLiteRecorder:
         c = self.conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='keystrokes';")
 
         if c.fetchone() is None:
-            print "Create another table"
             self.conn.execute("CREATE TABLE keystrokes (key, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")
-        else:
-            print "Table exists"
-            
+
     def record(self, keystroke):
         c = self.conn.execute("INSERT INTO keystrokes (key) values (?)", 
                               (keystroke,))
